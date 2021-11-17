6ffb3d2562f60d533cd5476d6f8666193c3ac5a5
tomiswinner <mouseofsack911@gmail.com>
2021-11-17 13:51:54 +0000

    implemented sidbar template

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,18 @@
 class BooksController < ApplicationController
+  def new
+    @book = Book.new()
+  end
+
   def create
     @book = Book.new(books_params)
     @book.save
     redirect_to(book_path(@book.id))
   end
-  
+
+  def index
+    @books = Book.all
+  end
+
   def show
     @book = Book.find(params[:id])
   end
