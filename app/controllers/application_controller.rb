51e802189c2bb2ed095c369f5ca8e03dcdcc7dbb
tomiswinner <mouseofsack911@gmail.com>
2021-11-17 12:19:08 +0000

    before db reset

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys:[:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys:[:name,:email,:password,:password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys:[:name,:password])
   end
 end
