df059c7b1eab575fbc63f44c600ac08521a0e8c2
tomiswinner <mouseofsack911@gmail.com>
2021-11-18 15:10:18 +0000

    tackling homes/about

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,11 @@
 class UsersController < ApplicationController
 
   def index
-    @book = Book.new
     @users = User.all
     @user = current_user
   end
 
   def show
-    @book = Book.new()
     @user = User.find(params[:id])
   end
 
