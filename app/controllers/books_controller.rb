466f6ce20e09e5185907269c9a3df592be7e92ff
tomiswinner <mouseofsack911@gmail.com>
2021-11-19 10:16:51 +0000

    implemented about page and desroy action

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -31,6 +31,12 @@ class BooksController < ApplicationController
     redirect_to(book_path(@book.id))
   end
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to(books_path)
+  end
+
   private
   def books_params
     params.require(:book).permit(:title,:body)
