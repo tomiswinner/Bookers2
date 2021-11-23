503cca3a51d5ed44043c6dd82881ae997256a4e3
tomiswinner <mouseofsack911@gmail.com>
2021-11-23 08:25:24 +0000

    set validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
   
-  validates :name, presence: true
+  validates :name, presence: true,length: {maximum: 20,minimum: 2},uniqueness: true
+  validates :introduction,length: {maximum: 50}
 
 end
